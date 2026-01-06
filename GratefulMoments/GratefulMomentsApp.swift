4cdfe185c50b5f1ebbb0aa8ad3f8c021fefcb7de
JohnAmadeo <john@modelcode.ai>
2026-01-06 10:32:07 +0700

    feat: implement gratitude journal with hexagonal moment views

--- GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -1,17 +1,17 @@
-//
-//  GratefulMomentsApp.swift
-//  GratefulMoments
-//
-//  Created by John Amadeo Daniswara on 1/6/26.
-//
-
 import SwiftUI
+import SwiftData
+
 
 @main
 struct GratefulMomentsApp: App {
+    let dataContainer = DataContainer()
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContainer)
         }
+        .modelContainer(dataContainer.modelContainer)
     }
 }
